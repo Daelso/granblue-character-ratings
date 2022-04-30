2e02857de5d3fbe783c1029a9d7d6e7c5bf464b9
Chase Murtaugh <Chasemurtaugh@gmail.com>
2022-04-30 19:30:53 -0400

    Adds JSON file with data, writes to the file

--- index.js
+++ index.js
@@ -1,39 +1,52 @@
-const PORT = 8000
+const PORT = 8000;
 
-const axios = require('axios');
-const cheerio = require('cheerio');
-const express = require('express');
+const axios = require("axios");
+const cheerio = require("cheerio");
+const express = require("express");
+const fs = require("fs");
 
-const scrapedURL = 'https://gbf.wiki/Character_Tier_List/Gamewith/Ratings'
+const scrapedURL = "https://gbf.wiki/Character_Tier_List/Gamewith/Ratings";
 
 const app = express();
 
 app.listen(PORT, () => console.log(`server running on PORT ${PORT}`));
 
-let characters = {
-    name: '',
-    rating: ''
+function scrape() {
+  let characters = {
+    name: "",
+    rating: "",
+  };
+
+  let characterarray = [];
+
+  axios(scrapedURL).then((response) => {
+    try {
+      const html = response.data;
+      const package = cheerio.load(html);
+
+      package(
+        "table.wikitable:nth-child(17) > tbody:nth-child(1) > tr",
+        html
+      ).each(function () {
+        const rating = package(this).find("td:nth-child(4)").text();
+        const imgTitle = package(this).find("a").attr("title");
+        characters = {
+          name: imgTitle,
+          rating: rating,
+        };
+        characterarray.push(characters);
+      });
+
+      let JSONarray = JSON.stringify(characterarray, null, 2);
+      fs.writeFile("db.json", JSONarray, function (err, result) {
+        if (err) {
+          console.log(err);
+        }
+      });
+    } catch (err) {
+      console.log(err);
+    }
+  });
 }
 
-let characterarray = []
-
-axios(scrapedURL)
-    .then(response => {
-        const html = response.data
-        const package = cheerio.load(html)
-        
-        package('table.wikitable:nth-child(17) > tbody:nth-child(1) > tr',  html).each(function(){
-            const rating = package(this).find('td:nth-child(4)').text()
-            const imgTitle = package(this).find('a').attr('title')
-            characters = {
-                name: imgTitle,
-                rating: rating
-            }
-            characterarray.push(characters)
-        })
-        
-
-        let JSONarray = JSON.stringify(characterarray, null, 2)
-        console.log(JSONarray)
-    })
-
+scrape();
